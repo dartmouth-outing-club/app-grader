5bd86a6ec69f2be173d46253168521ba9d105e34
Alexander Petros <contact@alexpetros.com>
2025-02-21 20:10:57 -0500

    Commit last year's move to making sure all apps are graded once

---
+++ scripts/init-db.js
@@ -0,0 +1,11 @@
+import fs from 'node:fs'
+import child_process from 'node:child_process'
+
+const DB_FILEPATH = process.env.DB_FILEPATH || './app-grader.db'
+
+if (!fs.existsSync(DB_FILEPATH)) {
+  console.log(`Database ${DB_FILEPATH} not found; creating schema`)
+  child_process.execSync(`sqlite3 ${DB_FILEPATH} ".read scripts/init-db.sql"`)
+} else {
+  console.log(`Database ${DB_FILEPATH} found; skipping initialization`)
+}
