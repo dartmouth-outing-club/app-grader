ad0ad68a32dbdc918bdc17cd40a11f83f2594263
Alexander Petros <contact@alexpetros.com>
2025-02-25 19:19:35 -0500

    No separate croo grading this year

--- src/functions/trips.js
+++ src/functions/trips.js
@@ -1,8 +1,10 @@
 // Ideally this would not be hardcoded, but we are out of time
-export const isLeaderApp = (fields) => {
-  return fields?.length > 3 && fields[3].response
+export const isLeaderApp = (_fields) => {
+  return true
+  // return fields?.length > 3 && fields[3].response
 }
 
-export const isCrooApp = (fields) => {
-  return fields?.length > 4 && fields[4].response
+export const isCrooApp = (_fields) => {
+  return false
+  // return fields?.length > 4 && fields[4].response
 }
